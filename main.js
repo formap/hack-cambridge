4523a93cad59404a98741d6d610319737cf42cdf
Florencia Tarditti <maria.florencia.tarditti@est.fib.upc.edu>
2017-01-28 15:11:12 +0000

    Globe loading model example

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
 
-camera.position.set(0, 0, 10);
-
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(new THREE.Color(0x00B2D6));
 
 window.addEventListener('resize', function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
+
+var player = new THREE.Object3D();
+scene.add(player);
 
 // Controls
 var controls = new THREE.OrbitControls( camera );
@@ -19,34 +22,35 @@ controls.maxDistance = 1000;
 controls.enablePan = false;
 
 // Our world
-var loader = new THREE.ColladaLoader();
+var loader = new THREE.OBJLoader();
+var matLoader = new THREE.MTLLoader();
+matLoader.setPath('models/');
+matLoader.load('globe.mtl', function(materials) {
+  loader.setMaterials(materials);
+  loader.load('models/globe.obj', function(object) {
+    object.rotation.y = Math.PI;
+    object.scale.set(0.5, 0.5, 0.5);
+    object.position.set(0, 0, 0);
+    //scene.add(object);
+    player.add(object);
+  });
+});
 
-loader.load(
-	// resource URL
-	'models/earth.dae',
-	// Function when resource is loaded
-	function ( collada ) {
-		scene.add( collada.scene );
-	},
-	// Function called when download progresses
-	function ( xhr ) {
-		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-	}
-);
+camera.position.set(0, 0, 20);
+
+scene.add(new THREE.AmbientLight(0xffffff));
 
 // Start!
 document.body.appendChild(renderer.domElement);
 render();
+animation();
 
 // Helpers
-function animate() {
-    requestAnimationFrame( animate );
-    controls.update();
+function animation() {
+    controls.update(0.1);
 }
 
 function render() {
     requestAnimationFrame(render);
     renderer.render(scene, camera);
 }
-
-animate();
